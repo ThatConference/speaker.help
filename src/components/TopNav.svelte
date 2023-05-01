d57d9b2d51369454a91fa3f63ea0409ca574a090
Clark Sell <clark.sell@csell.net>
2023-05-01 08:05:23 -0500

    feat: logo and font updates

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -5,11 +5,11 @@
 <AppBar>
 	<svelte:fragment slot="lead">
 		<div class="pl-4">
-			<img class="h-12" src="/images/speaker-help-icon-red.svg" alt="WebSite Logo" />
+			<img class="h-12" src="/images/speaker-help-icon-red-right.svg" alt="WebSite Logo" />
 		</div>
 	</svelte:fragment>
 
-	<h2>Speaker Help</h2>
+	<h2 class="-ml-2">Speaker Help</h2>
 
 	<svelte:fragment slot="trail">
 		<div class="flex space-x-4 pr-4">
